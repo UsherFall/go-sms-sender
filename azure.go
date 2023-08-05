b02c664be047a9de7cb084323a32cd58726fb6d6
UsherFall <1270329076@qq.com>
2023-08-06 00:18:05 +0800

    fix azure acs

--- azure.go
+++ azure.go
@@ -28,6 +28,17 @@ type ACSClient struct {
 	template string
 }
 
+type ACSResponse struct {
+	Value []struct {
+		To                  string `json:"to"`
+		MessageID           string `json:"messageId"`
+		HTTPStatus          int    `json:"httpStatusCode"`
+		ErrorMessage        string `json:"errorMessage"`
+		RepeatabilityResult string `json:"repeatabilityResult"`
+		Successful          bool   `json:"successful"`
+	} `json:"value"`
+}
+
 func GetACSClient(apiKey string, template string, endpoint []string) (*ACSClient, error) {
 	if len(endpoint) < 1 {
 		return nil, fmt.Errorf("missing parameter: endpoint")
@@ -55,34 +66,57 @@ func (a *ACSClient) SendMessage(param map[string]string, targetPhoneNumber ...st
 	}
 
 	url := fmt.Sprintf("%s/sms?api-version=2021-03-07", a.endpoint)
+
+	// 构建请求体
 	payload := map[string]interface{}{
-		"from": fromPhoneNumber,
-		"body": message,
+		"from":    fromPhoneNumber,
+		"message": message,
+		"smsSendOptions": map[string]interface{}{
+			"enableDeliveryReport": true,
+		},
+		"smsRecipients": []map[string]interface{}{},
+	}
+
+	for i := 1; i < len(targetPhoneNumber); i++ {
+		payload["smsRecipients"] = append(payload["smsRecipients"].([]map[string]interface{}), map[string]interface{}{
+			"to": targetPhoneNumber[i],
+		})
+	}
+
+	requestBody, err := json.Marshal(payload)
+	if err != nil {
+		return fmt.Errorf("error creating request body: %w", err)
 	}
 
 	client := &http.Client{}
-	for i := 1; i < len(targetPhoneNumber); i++ {
-		payload["to"] = targetPhoneNumber[i]
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(requestBody))
+	if err != nil {
+		return fmt.Errorf("error creating request: %w", err)
+	}
 
-		requestBody, err := json.Marshal(payload)
-		if err != nil {
-			return fmt.Errorf("error creating request body: %w", err)
+	req.Header.Add("Authorization", "Bearer "+a.apiKey)
+	req.Header.Add("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("error sending request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("message sending failed")
+	}
+
+	var acsResp ACSResponse
+	err = json.NewDecoder(resp.Body).Decode(&acsResp)
+	if err != nil {
+		return fmt.Errorf("error parsing response body: %w", err)
+	}
+
+	for _, msg := range acsResp.Value {
+		if !msg.Successful {
+			return fmt.Errorf("message sending failed for target phone number %s", msg.To)
 		}
-
-		req, err := http.NewRequest("POST", url, bytes.NewBuffer(requestBody))
-		if err != nil {
-			return fmt.Errorf("error creating request: %w", err)
-		}
-
-		req.Header.Add("Authorization", "Bearer "+a.apiKey)
-		req.Header.Add("Content-Type", "application/json")
-
-		resp, err := client.Do(req)
-		if err != nil {
-			return fmt.Errorf("error sending request: %w", err)
-		}
-
-		resp.Body.Close()
 	}
 
 	return nil
